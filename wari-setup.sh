18f04e7556dd48311b67f7ea1dcf858a7fd5806b
Pete Peterson <peterfpeterson@gmail.com>
2016-01-13 12:31:25 -0500

    Example with multiple config files

--- wari-setup.sh
+++ wari-setup.sh
@@ -20,9 +20,21 @@ if [ -f "/etc/os-release" ]; then
   . /etc/os-release
   WARI_DISTRO=$ID
   WARI_DISTRO_VERSION=$VERSION_ID
-else 
+else
   echo "/etc/os-release not available - your distro is too old! Upgrade! :-)"
   exit -1
 fi
 
+if [ -f "$WARI_ROOT/wari-repos" ]; then
+  source $WARI_ROOT/wari-repos
+else
+  echo "Did not find 'wari-repos'...skipping"
+fi
+source $WARI_ROOT/wari-addrepo-yum.sh $WARI_REPOS
 
+if [ -f "$WARI_ROOT/wari-packages" ]; then
+  source $WARI_ROOT/wari-packages
+else
+  echo "Did not find 'wari-packages'...skipping"
+fi
+source $WARI_ROOT/wari-addpackages.sh $WARI_PACKAGES
